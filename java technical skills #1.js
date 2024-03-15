bc4975ac885552d800564e76ef932383bffb4261
donavonmanning1 <156944357+donavonmanning@users.noreply.github.com>
2024-03-15 18:39:17 -0400

    Add files via upload

---
+++ java technical skills #1.js
@@ -0,0 +1,10 @@
+let partyLocation = "GARDEN";
+const shiftValue = +3;
+let encryptedMessage = "JDUGHQ";
+let decryptedMessager = "GARDEN"
+Number .isInteger(shiftValue);
+
+JDUGHQ = partyLocation +- shiftValue;
+let example = "ABC" + shiftValue == "DEF";
+
+console.log(JDUGHQ);
